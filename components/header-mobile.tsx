c79b4c1ab5ea6e0bd52d8143b3e6cad686d037d1
Francy Santos <francy.horray@gmail.com>
2024-03-20 21:42:04 -0300

    feat: add project setup, header & header mobile

---
+++ components/header-mobile.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const links = [
+  { label: "Sobre", href: "/sobre" },
+  { label: "Projetos", href: "/sobre" },
+  { label: "Baixar CV", href: "/cv-download" },
+];
+
+const HeaderMobile = () => {
+  return (
+    <nav className=" md:flex gap-6">
+      <ul className="flex flex-col w-full">
+        {links.map((link) => (
+          <Link key={link.label} href={link.href}>
+            <li className="py-6 text-center text-mainBlack font-medium text-md hover:text-purple transition-all duration-500 border-b">
+              {link.label}
+            </li>
+          </Link>
+        ))}
+      </ul>
+      <div className="flex gap-5">
+        <Image
+          src={"img/icons/globe-i.svg"}
+          alt="Language"
+          width={25}
+          height={25}
+        />
+        <Image
+          src={"img/icons/moon-on-i.svg"}
+          alt="Language"
+          width={25}
+          height={25}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderMobile;
---
+++ components/header.tsx
@@ -0,0 +1,100 @@
+import { link } from "fs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import HeaderMobile from "./header-mobile";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+const links = [
+  { label: "Sobre", href: "/sobre" },
+  { label: "Projetos", href: "/sobre" },
+  { label: "Baixar CV", href: "/cv-download" },
+];
+
+const Header = () => {
+  return (
+    <header className="line w-full h-[95px] items-center justify-between flex mx-auto max-w-[1045px]">
+      <Link href={"/"} className="py-5">
+        <Image
+          src={"img/brand/fsantos-logo.svg"}
+          alt="Logo"
+          width={103}
+          height={200}
+        />
+      </Link>
+
+      {/* Menu */}
+      <nav className="hidden md:flex gap-6">
+        <ul className="flex gap-3 w-full">
+          {links.map((link) => (
+            <Link key={link.label} href={link.href}>
+              <li className="w-[140px] py-3 text-center text-white font-medium text-md hover:text-purple  hover:after:content-['/>'] after:pl-[5px] hover:before:content-['<'] before:pl-[5px] transition-all duration-500">
+                {link.label}
+              </li>
+            </Link>
+          ))}
+        </ul>
+        <div className="flex gap-5">
+          <Image
+            src={"img/icons/globe-i.svg"}
+            alt="Language"
+            width={25}
+            height={25}
+          />
+          <Image
+            src={"img/icons/moon-on-i.svg"}
+            alt="Language"
+            width={25}
+            height={25}
+          />
+        </div>
+      </nav>
+
+      {/* Menu Mobile */}
+      <div className="md:hidden flex">
+        <Sheet>
+          <SheetTrigger>
+            {/* <MenuIcon className="text-white" /> */}
+            <Button variant={"ghost"}>
+              <Image
+                src={"img/icons/menu-mobile-i.svg"}
+                alt="Open menu"
+                width={25}
+                height={25}
+              />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="sm:w-[540px]">
+            <SheetHeader>
+              <SheetTitle className="pt-8 flex flex-col items-center justify-center">
+                <Link href={"/"} className="py-5">
+                  <Image
+                    src={"img/brand/fsantos-logo.svg"}
+                    alt="Logo"
+                    width={103}
+                    height={200}
+                    color="black"
+                  />
+                </Link>
+              </SheetTitle>
+            </SheetHeader>
+            {/* HEADER MOBILE */}
+            <HeaderMobile />
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
