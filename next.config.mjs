74f4c1e63732a0b647e9d0ebf23b1c0f466a65a1
Francy Santos <francy.horray@gmail.com>
2024-04-15 20:38:10 -0300

    feat: rewrite config v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
-import path from "path";
-
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-
-const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "sass")],
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "bio.francy.dev",
+          },
+        ],
+        destination: "/app/bio",
+        permanent: true,
+      },
+    ];
   },
 };
-
-export default nextConfig;
